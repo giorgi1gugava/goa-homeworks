b276e22d8029428c520fc6b55a85d0dc7f11ea33
giorgi1gugava <gugavag458@gmail.com>
2024-10-14 20:19:33 +0400

    third

---
+++ day03/homework/homework.py
@@ -0,0 +1,18 @@
+#pirveli
+name = "gio"
+saurname = "gugava"
+#pirvel rigsi davwere mnisvneloba name semdeg speisi tolobis nisani speisi frcxilebi da signit movaqcie gio
+
+#meore
+height = 190
+age = 16
+print(name, saurname, height, age)
+
+#mesame
+naame = "giorgi"
+io = "cemi"
+ip = "saxelia"
+print(io, ip, naame)
+
+#meotxe
+#cvladi aris mexsiereba sadac segvizlia sevinaxot mnisvneloba da gamovicenot mere
