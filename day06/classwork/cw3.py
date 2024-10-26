225717633540535d08a72748968bbbe7501b573a
giorgi1gugava <gugavag458@gmail.com>
2024-10-26 19:41:52 +0400

    sixth commit

---
+++ day06/classwork/cw3.py
@@ -0,0 +1,15 @@
+name = "giorgi"
+saurname = "gugava"
+age = 17
+
+print("my name is "+ "" + name + " " + "my saurname is " + saurname  +  " and age is " +  str(age))
+
+num = 10
+num1 = 1.2
+num2 = 15
+
+number = num
+number2 = num2
+number3 = num1 
+
+
---
+++ day06/homework/hw4.py
@@ -0,0 +1,8 @@
+#დაწერეთ პროგრამა სადაც მომხმარებელს შემოატანინებთ ინფორმაციას თავის თავზე (name,surname,age,address,gmail) და შემდეგ დაუპრინტეთ ეს ინფორმაცია ერთ დიდ წინადადებად
+
+name = "dato"
+saurname = "liparteliani"
+age = 30
+gmail = "dato@gmail.com"
+
+print("misi saxelia " + name + " gvaria " + saurname + " asakia " + str(age) + " weli" + " da emailze aweria " + gmail)
