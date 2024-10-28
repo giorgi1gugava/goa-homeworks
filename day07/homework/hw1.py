996d0f7ca4eb2d7fb2cf3446a6c9a3b133ce3297
giorgi1gugava <gugavag458@gmail.com>
2024-10-28 20:41:09 +0400

    seventh commit

---
+++ day07/homework/hw1.py
@@ -0,0 +1,5 @@
+#შექნენით 2 ცვლადი თქვენი სახელი და გვარი მოახდინეთ კონკატენაცი
+name = "gio"
+saurname = "gugava"
+
+print(name + "." + saurname)
---
+++ day07/homework/hw10.py
@@ -0,0 +1,9 @@
+#გაიხსენეთ type ფუნქცია და განახორციელეთ გარდაქმნები მონაცემთა ტიპებისა ცვლადში
+num = "14"
+age = 30
+
+print(type(num))
+print(type(age))
+
+print(int(num))
+print(str(age))
