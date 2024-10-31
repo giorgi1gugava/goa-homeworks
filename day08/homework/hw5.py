8801d41013089af184af3a7330880f861fbf57a7
giorgi1gugava <gugavag458@gmail.com>
2024-10-31 20:41:28 +0400

    eight commit

---
+++ day08/homework/hw5.py
@@ -0,0 +1,14 @@
+#num1 = 6
+#num2 = 8
+#num3 = 10
+#average = num1 + num2 + num3 / 3
+#print("Average:", average)
+
+#შეასწორეთ კოდი ისე რომ საშუალო არითმეტიკული სწორად გამოითვალოს
+
+#ამოხსნა:
+num1 = 6
+num2 = 8
+num3 = 10
+average =(num1 + num2 + num3) / 3
+print(average)
