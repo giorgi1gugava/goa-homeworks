e7a17f6d1658ffe82083107b8ae0d9c54824aac7
giorgi1gugava <gugavag458@gmail.com>
2024-11-12 15:34:39 +0400

    eleventh commit

---
+++ bank/bank2.py/2.py
@@ -0,0 +1,39 @@
+#სახელი: გიორგი
+#პირადი ნომერი: 1234
+#ბალანსი ანგარიშზე: 50ლარი
+
+#გიორგიმ უნდა ჩარიცხოს 50ლარი საკუთარ ანგარიშზე,ბანკომატის გამოყენებით.
+
+
+
+#1)
+code = input("akrife seni piradi nomeri :")
+#seni piradi nomeria:1234
+
+
+
+#2)
+code1 = input("seiyvane esemes kodi (esemes kodi gamoigzavneba nomerze): ")
+#esemes kodi : 2233
+
+
+
+#3)
+code2 = input("airciet angarisi tanxis sesatanad: 1-sakutari angaris. 2-sxvisi angarisi. :")
+#vircevt sakutar angariss:1
+
+
+
+#4)
+code3 = input("aircie angarisi:1-IG***400: ")
+#angarisi IG***400 
+
+
+
+#5)
+code4= input("0.00ლ seitane tanxa:")
+#ბალანსი ანგარიშზე:50ლარი
+
+
+print("1)sworia seni piradi nomeria:" + code +" "+ "2)esemes kodia:" + code1 +" "+ "3)tqven airciet sakutari angarisi-"
++ code2 +" "+ "4)tqven airciet angarisi-IG*****400"+" "+ "5)setanilia-50ლ" +" "+ "6)tranzlaqcia sesrulebulia.")
